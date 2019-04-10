e293b686b3addebfb1b0a03b8a9ac18f3421014a
Evan Bacon <baconbrix@gmail.com>
2019-04-10 15:55:35 -0700

    Updated header for web
    * Using material SVG for back button due to safari image tint bug https://github.com/necolas/react-native-web/issues/1319
    * Updated header bar height to be 64px
    * Updated header title style to match material color, size, weight
    * Changed how header margins work on web
    * Removed assets on web

--- src/views/Header/Header.js
+++ src/views/Header/Header.js
@@ -17,12 +17,26 @@ import HeaderBackButton from './HeaderBackButton';
 import ModularHeaderBackButton from './ModularHeaderBackButton';
 import HeaderStyleInterpolator from './HeaderStyleInterpolator';
 
-const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
-const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : 0;
+const APPBAR_HEIGHT = Platform.select({
+  ios: 44,
+  android: 56,
+  default: 64,
+});
+const STATUSBAR_HEIGHT = Platform.select({
+  ios: 20,
+  default: 0,
+});
 
 // These can be adjusted by using headerTitleContainerStyle on navigationOptions
-const TITLE_OFFSET_CENTER_ALIGN = Platform.OS === 'ios' ? 70 : 56;
-const TITLE_OFFSET_LEFT_ALIGN = Platform.OS === 'ios' ? 20 : 56;
+const TITLE_OFFSET_CENTER_ALIGN = Platform.select({
+  ios: 70,
+  default: 56,
+});
+
+const TITLE_OFFSET_LEFT_ALIGN = Platform.select({
+  ios: 20,
+  default: 56,
+});
 
 const getTitleOffsets = (
   layoutPreset,
@@ -62,11 +76,17 @@ const getTitleOffsets = (
 };
 
 const getAppBarHeight = isLandscape => {
-  return Platform.OS === 'ios'
-    ? isLandscape && !Platform.isPad
-      ? 32
-      : 44
-    : 56;
+  if (Platform.OS === 'ios') {
+    if (isLandscape && !Platform.isPad) {
+      return 32;
+    } else {
+      return 44;
+    }
+  } else if (Platform.OS === 'android') {
+    return 56;
+  } else {
+    return 64;
+  }
 };
 
 class Header extends React.PureComponent {
@@ -676,6 +696,10 @@ const styles = StyleSheet.create({
   header: {
     ...StyleSheet.absoluteFillObject,
     flexDirection: 'row',
+    ...Platform.select({
+      default: {},
+      web: { marginHorizontal: 16 },
+    }),
   },
   item: {
     backgroundColor: 'transparent',
--- src/views/Header/HeaderBackButton.js
+++ src/views/Header/HeaderBackButton.js
@@ -11,14 +11,19 @@ import {
 import TouchableItem from '../TouchableItem';
 
 import defaultBackImage from '../assets/back-icon.png';
+import BackButtonWeb from './BackButton.web';
 
 class HeaderBackButton extends React.PureComponent {
   static defaultProps = {
     pressColorAndroid: 'rgba(0, 0, 0, .32)',
     tintColor: Platform.select({
       ios: '#037aff',
+      web: '#5f6368',
     }),
     truncatedTitle: 'Back',
+    backImage: Platform.select({
+      web: BackButtonWeb,
+    }),
   };
 
   state = {};
