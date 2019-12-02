49eeab054ca1789185db666514069baadea0e4e8
Radek Czemerys <radko93@gmail.com>
2019-12-02 21:19:06 +0100

    Set proper pointerEvents for focused sreen

--- src/views/Header/HeaderContainer.tsx
+++ src/views/Header/HeaderContainer.tsx
@@ -109,7 +109,9 @@ export default function HeaderContainer({
                       })
                   : undefined
               }
-              pointerEvents="box-none"
+              pointerEvents={
+                isFocused ? 'box-none' : 'none'
+              }
               accessibilityElementsHidden={!isFocused}
               importantForAccessibility={
                 isFocused ? 'auto' : 'no-hide-descendants'
